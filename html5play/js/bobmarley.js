c7d2a8b35d086beea82e35f13e8fffaabd8303e3
rllom <rllom1970@gmail.com>
2024-08-18 16:29:04 -0600

    update

---
+++ html5play/js/bobmarley.js
@@ -0,0 +1,156 @@
+/**
+ * Created by 23rd and Walnut
+ * www.23andwalnut.com
+ * User: Saleem El-Amin
+ * Date: 6/8/11
+ * Time: 9:39 AM
+ */
+
+var myPlaylist = [
+
+{
+mp3:"http://saigonocean3.com/nghenhacBobMarley/songs/1.mp3",
+title:"Is This Love",
+artist:"Bob Marley & The Wailers",
+buy:"http://saigonocean3.com/nghenhacBobMarley/songs/1.mp3",
+cover:"https://images2.imgbox.com/c6/7c/OBAJVzdD_o.jpg"
+},
+
+{
+mp3:"http://saigonocean3.com/nghenhacBobMarley/songs/9.mp3",
+title:"One Love",
+artist:"Bob Marley & The Wailers",
+buy:"http://saigonocean3.com/nghenhacBobMarley/songs/9.mp3",
+cover:"https://images2.imgbox.com/28/5c/z3qS7NIP_o.jpg"
+},
+
+{
+mp3:"https://saigonocean3.com/nghenhacBobMarley/songs/2.mp3",
+title:"No Women No Cry",
+artist:"Bob Marley & The Wailers",
+buy:"https://saigonocean3.com/nghenhacBobMarley/songs/2.mp3",
+cover:"https://images2.imgbox.com/5e/83/68CRhBUG_o.jpg",
+},
+
+{
+mp3:"https://saigonocean3.com/nghenhacBobMarley/songs/3.mp3",
+title:"Could You Be Loved",
+artist:"Bob Marley & The Wailers",
+buy:"https://saigonocean3.com/nghenhacBobMarley/songs/3.mp3",
+cover:"https://images2.imgbox.com/8c/44/dBEmDDhC_o.jpg",
+},
+
+{
+title:"Three Little Birds",
+artist:"Bob Marley & The Wailers  ",
+mp3:"http://saigonocean3.com/nghenhacBobMarley/songs/4.mp3",	
+buy:"http://saigonocean3.com/nghenhacBobMarley/songs/4.mp3",
+cover:"https://images2.imgbox.com/8f/98/43wUae7h_o.jpg",
+},
+
+{
+title:"Buffalo Soldier",
+artist:"Bob Marley & The Wailers ",
+mp3:"http://saigonocean3.com/nghenhacBobMarley/songs/5.mp3",
+buy:"http://saigonocean3.com/nghenhacBobMarley/songs/5.mp3",	
+cover:"https://images2.imgbox.com/fd/9d/ppinIEOg_o.jpg",
+},
+
+{
+title:"Get Up, Stand Up ",
+artist:"Bob Marley & The Wailers  ",
+mp3:"http://saigonocean3.com/nghenhacBobMarley/songs/6.mp3",			
+buy:"http://saigonocean3.com/nghenhacBobMarley/songs/6.mp3",			
+cover:"https://images2.imgbox.com/7f/67/ynFeZyvY_o.jpg",
+},
+
+{
+title:"Stir It Up",
+artist:"  Bob Marley & The Wailers  ",
+mp3:"http://saigonocean3.com/nghenhacBobMarley/songs/7.mp3",
+buy:"http://saigonocean3.com/nghenhacBobMarley/songs/7.mp3",			
+cover:"https://images2.imgbox.com/d8/c1/KYYe4z3Q_o.jpg",
+},
+
+{
+title:" Easy Skanking   ",
+artist:"  Bob Marley & The Wailers  ",
+mp3:"http://saigonocean3.com/nghenhacBobMarley/songs/8.mp3",
+buy:"http://saigonocean3.com/nghenhacBobMarley/songs/8.mp3",			
+cover:"https://images2.imgbox.com/32/0e/8gBWfHcH_o.jpg",
+},
+
+{
+title:"I Shot The Sheriff  ",
+free:true,
+artist:"  Bob Marley & The Wailers  ",
+mp3:"http://saigonocean3.com/nghenhacBobMarley/songs/10.mp3",	
+buy:"http://saigonocean3.com/nghenhacBobMarley/songs/10.mp3",			
+cover:"https://images2.imgbox.com/60/f2/1bdspXpG_o.jpg",
+},
+
+{
+title:" Waiting In Vain ",
+artist:"  Bob Marley & The Wailers  ",
+mp3:"http://saigonocean3.com/nghenhacBobMarley/songs/11.mp3",
+buy:"http://saigonocean3.com/nghenhacBobMarley/songs/11.mp3",			
+cover:"https://images2.imgbox.com/af/15/HCUyzz0F_o.jpg",
+},
+
+{
+title:" Redemption Song ",
+artist:"  Bob Marley & The Wailers  ",
+mp3:"http://saigonocean3.com/nghenhacBobMarley/songs/12.mp3",	
+buy:"http://saigonocean3.com/nghenhacBobMarley/songs/12.mp3",			
+cover:"https://images2.imgbox.com/c6/7c/OBAJVzdD_o.jpg",
+},
+
+{
+title:"  Satisfy My Soul  ",
+artist:"  Bob Marley & The Wailers  ",
+mp3:"http://saigonocean3.com/nghenhacBobMarley/songs/14.mp3",
+buy:"http://saigonocean3.com/nghenhacBobMarley/songs/14.mp3",			
+cover:"https://images2.imgbox.com/28/5c/z3qS7NIP_o.jpg",
+},
+
+{
+title:" Exodus  ",
+artist:"  Bob Marley & The Wailers  ",
+mp3:"http://saigonocean3.com/nghenhacBobMarley/songs/13.mp3",	
+buy:"http://saigonocean3.com/nghenhacBobMarley/songs/13.mp3",	
+cover:"https://images2.imgbox.com/8c/44/dBEmDDhC_o.jpg",
+},
+
+{
+title:" Jammin ",
+artist:"  Bob Marley & The Wailers  ",
+mp3:"http://saigonocean3.com/nghenhacBobMarley/songs/15.mp3",	
+buy:"http://saigonocean3.com/nghenhacBobMarley/songs/15.mp3",			
+cover:"https://images2.imgbox.com/8f/98/43wUae7h_o.jpg",
+},
+
+{
+title:" Punky Reggae Party ",
+artist:"  Bob Marley & The Wailers  ",
+mp3:"http://saigonocean3.com/nghenhacBobMarley/songs/16.mp3",	
+buy:"http://saigonocean3.com/nghenhacBobMarley/songs/16.mp3",			
+cover:"https://images2.imgbox.com/fd/9d/ppinIEOg_o.jpg",
+},
+
+{
+title:" Duppy Conqueror ",
+artist:"  Bob Marley & The Wailers  ",
+mp3:"http://saigonocean3.com/nghenhacBobMarley/songs/17.mp3",	
+buy:"http://saigonocean3.com/nghenhacBobMarley/songs/17.mp3",			
+cover:"https://images2.imgbox.com/32/0e/8gBWfHcH_o.jpg",
+},
+
+{
+title:" Trench Town Rock   ",
+artist:"  Bob Marley & The Wailers  ",
+mp3:"http://saigonocean3.com/nghenhacBobMarley/songs/18.mp3",	
+buy:"http://saigonocean3.com/nghenhacBobMarley/songs/18.mp3",			
+cover:"https://images2.imgbox.com/60/f2/1bdspXpG_o.jpg",
+}
+
+];
---
+++ html5play/js/myplaylist.js
@@ -0,0 +1,27 @@
+/**
+ * Created by 23rd and Walnut
+ * www.23andwalnut.com
+ * User: Saleem El-Amin
+ * Date: 6/8/11
+ * Time: 9:39 AM
+ */
+
+var myPlaylist = [
+
+{
+mp3:"http://saigonocean3.com/nghenhacBobMarley/songs/1.mp3",
+title:"Is This Love",
+artist:"Bob Marley & The Wailers",
+buy:"http://saigonocean3.com/nghenhacBobMarley/songs/1.mp3",
+cover:"https://images2.imgbox.com/c6/7c/OBAJVzdD_o.jpg"
+},
+
+{
+mp3:"http://saigonocean3.com/nghenhacBobMarley/songs/9.mp3",
+title:"One Love",
+artist:"Bob Marley & The Wailers",
+buy:"http://saigonocean3.com/nghenhacBobMarley/songs/9.mp3",
+cover:"https://images2.imgbox.com/28/5c/z3qS7NIP_o.jpg"
+},
+
+];
